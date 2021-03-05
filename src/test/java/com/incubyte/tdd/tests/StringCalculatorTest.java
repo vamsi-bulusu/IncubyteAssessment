0db2989ec70769302a1cd0e169d47b1548406348
vamsikrishna1999 <bulusu.vk08@gmail.com>
2021-03-06 01:00:24 +0530

    Added new Testcase with newline as Delimiter between numbers

--- src/test/java/com/incubyte/tdd/tests/StringCalculatorTest.java
+++ src/test/java/com/incubyte/tdd/tests/StringCalculatorTest.java
@@ -11,19 +11,25 @@ public class StringCalculatorTest {
     @Test
     void addEmptyString(){
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, stringCalculator.add(""),"Empty string returns 0");
     }
 
     @Test
     void addStringWithOneNum(){
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
+        assertEquals(1, stringCalculator.add("1"), "1 == 1");
     }
 
     @Test
     void addStringWithTwoNum(){
         StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(3, stringCalculator.add("1,2"));
+        assertEquals(3, stringCalculator.add("1,2"), "1,2 == 1+2");
+    }
+
+    @Test
+    void addStringWithNewline(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(6, stringCalculator.add("1\n2,3"), "Newline between numbers instead of commas");
     }
 
 }

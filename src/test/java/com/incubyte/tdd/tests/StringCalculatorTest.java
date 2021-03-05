91e0ce6aea9540502d69826f31aafeee6073ed48
vamsikrishna1999 <bulusu.vk08@gmail.com>
2021-03-06 00:13:54 +0530

    Added new Testcase - string containing two numbers separated by "," as Delimiter

--- src/test/java/com/incubyte/tdd/tests/StringCalculatorTest.java
+++ src/test/java/com/incubyte/tdd/tests/StringCalculatorTest.java
@@ -13,9 +13,17 @@ public class StringCalculatorTest {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(0, stringCalculator.add(""));
     }
+
     @Test
     void addStringWithOneNum(){
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+
+    @Test
+    void addStringWithTwoNum(){
+        StringCalculator stringCalculator = new StringCalculator();
+        assertEquals(1, stringCalculator.add("1,2"));
+    }
+
 }

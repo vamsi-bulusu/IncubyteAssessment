3b839b7894331c28112ef74380d8607c9dfd8f23
vamsikrishna1999 <bulusu.vk08@gmail.com>
2021-03-06 21:53:27 +0530

    Refactored add functionality to tokenize custom delimiter.

--- src/main/java/com/incubyte/tdd/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/StringCalculator.java
@@ -1,16 +1,34 @@
 package com.incubyte.tdd;
 
 import java.util.InputMismatchException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
 
 public class StringCalculator {
 
+    public int add(String number){
+        if(number.isEmpty()) return 0;
+        else {
+            return sum(Stream.of(tokenize(number)).mapToInt(Integer::parseInt).toArray());
+        }
+    }
 
     public String[] tokenize(String number){
+        if(number.startsWith("//")) return customTokenize(number);
         if(number.contains(",\n") || number.contains("\n,")){
             throw new InputMismatchException("Invalid Input");
         }
         return number.split(",|\n");
+    }
+
+    public String[] customTokenize(String number){
+        Pattern pattern = Pattern.compile("//(.)\n(.*)");
+        Matcher matcher = pattern.matcher(number);
+        boolean isMatch = matcher.matches();
+        String delimiter = matcher.group(1);
+        return matcher.group(2).split(Pattern.quote(delimiter));
     }
 
     public int sum(int[] numbers){
@@ -19,12 +37,4 @@ public class StringCalculator {
         return result;
     }
 
-
-    public int add(String number){
-        if(number.isEmpty()) return 0;
-        else {
-            int[] numbers = Stream.of(tokenize(number)).mapToInt(Integer::parseInt).toArray();
-            return sum(numbers);
-        }
-    }
 }

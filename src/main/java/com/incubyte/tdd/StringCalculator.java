1d6c0842741c2cd21d4df39914da0b0b3651430c
vamsikrishna1999 <bulusu.vk08@gmail.com>
2021-03-06 19:01:32 +0530

    Refactored add functionality to separate out concerns and added check for valid input.

--- src/main/java/com/incubyte/tdd/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/StringCalculator.java
@@ -1,15 +1,29 @@
 package com.incubyte.tdd;
 
+import java.util.InputMismatchException;
 import java.util.stream.Stream;
 
 public class StringCalculator {
-    public int add(String number){
-        if(number.equals("")) return 0;
-        int[] numbers = Stream.of(number.split("[,\\n]")).mapToInt(Integer::parseInt).toArray();
-        int sum = 0;
-        for (int num : numbers) {
-            sum += num;
+
+
+    public String[] tokenize(String number){
+        if(number.contains(",\n") || number.contains("\n,")){
+            throw new InputMismatchException("Invalid Input");
         }
-        return sum;
+        return number.split(",|\n");
+    }
+    public int sum(int[] numbers){
+        int result = 0;
+        for (int num:numbers) result += num;
+        return result;
+    }
+
+
+    public int add(String number){
+        if(number.isEmpty()) return 0;
+        else {
+            int[] numbers = Stream.of(tokenize(number)).mapToInt(Integer::parseInt).toArray();
+            return sum(numbers);
+        }
     }
 }

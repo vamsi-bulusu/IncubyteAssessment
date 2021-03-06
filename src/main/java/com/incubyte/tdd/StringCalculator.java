ea76b1f72a4b7840a49ae66b88fbe38b06b0a89f
vamsikrishna1999 <bulusu.vk08@gmail.com>
2021-03-06 17:10:59 +0530

    Refactored add functionality to match both newline and comma as delimiters

--- src/main/java/com/incubyte/tdd/StringCalculator.java
+++ src/main/java/com/incubyte/tdd/StringCalculator.java
@@ -5,7 +5,7 @@ import java.util.stream.Stream;
 public class StringCalculator {
     public int add(String number){
         if(number.equals("")) return 0;
-        int[] numbers = Stream.of(number.split(",")).mapToInt(Integer::parseInt).toArray();
+        int[] numbers = Stream.of(number.split("[,\\n]")).mapToInt(Integer::parseInt).toArray();
         int sum = 0;
         for (int num : numbers) {
             sum += num;
